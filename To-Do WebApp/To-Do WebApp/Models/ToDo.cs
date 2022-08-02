059ca0f264579c9fba15333735ad81675536aed4
johnthebeloved <johnoluwale1@gmail.com>
2022-08-02 15:53:07 -0400

    ToDo class for SQLite Database Creation

---
+++ To-Do WebApp/To-Do WebApp/Models/ToDo.cs
@@ -0,0 +1,15 @@
+﻿namespace To_Do_WebApp.Models
+{
+    public class ToDo
+    {
+        /**
+     *SQlite will build the entity and attributes
+     *usiing the variables declared in this class
+     */
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Boolean isDone { get; set; }
+        public DateTime AddDate { get; set; }
+        public List<ToDo> ToDoList { get; set; }
+    }
+}
