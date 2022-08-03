3a37347255bbb7a7fb82fe270c5add6a4a50b7f5
johnthebeloved <johnoluwale1@gmail.com>
2022-08-03 14:44:44 -0400

    Update ToDoContext.cs

--- To-Do WebApp/To-Do WebApp/Data/ToDoContext.cs
+++ To-Do WebApp/To-Do WebApp/Data/ToDoContext.cs
@@ -5,24 +5,15 @@ namespace To_Do_WebApp.Data
 {
     public class ToDoContext : DbContext
     {
-        public DbSet<ToDo> toDo { get; set; }
-
-        public string DbPath { get; }
-
         /**
          *Constructor that handles the path for storing
          *the database file locally
          */
-        public ToDoContext()
+        public ToDoContext(DbContextOptions<ToDoContext> options) : base(options)
         {
-            var folder = Environment.SpecialFolder.LocalApplicationData;
-            var path = Environment.GetFolderPath(folder);
-            DbPath = System.IO.Path.Join(path, "mytodo.db");
+
         }
 
-        //Configuring EF to create an sqlite database file in the
-        //special local folder 
-        protected override void OnConfiguring(DbContextOptionsBuilder options)
-            => options.UseSqlite($"Data Source = {DbPath}");
+        public DbSet<ToDo> ToDo { get; set; }
     }
 }
